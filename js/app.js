d2da8bb506416aae30f003d7571334c6eef4ec24
万洲 <wanzhou.njust2010@gmail.com>
2014-09-19 15:03:52 +0800

    19th

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+
+    var documentHeight = 0;
+    var $cate = $("#categorybar").offset(), 
+        $auth = $('#authorslistbar').offset(),
+        $newbar = $('#newbar').offset();
+    var offset = $cate != null ? $cate :
+             $auth != null ? $auth :
+             $newbar != null ? $newbar : null;
+    var $bar = $cate != null ? $('#categorybar') :
+               $auth != null ? $('#authorslistbar') :
+               $newbar != null ? $('#newbar') : null;
+
+    documentHeight = $(document).height();
+
     (function($){
         $('[data-toggle="tooltip"]').tooltip();
         $('table').addClass('table table-bordered table-striped table-hover');
@@ -6,14 +20,37 @@ $(document).ready(function(){
         $('table tr th').addClass('success');
         $('p > img').wrap('<a class="thumbnail"></a>');
     })(jQuery);
+
     $(window).scroll(function(){
         var scrollh = $(window).scrollTop();
+       
         if( scrollh > 200 ){
             $('#to-top, #to-top-xs').fadeIn('fast');
         }else{
             $('#to-top, #to-top-xs').stop().fadeOut('fast');
         }
+
+        if( offset && $bar ){
+            //console.log(offset);
+            //console.log(scrollh);
+            //console.log($bar);
+            if( scrollh > offset.top ){
+                var newPosition = scrollh - offset.top + 13,
+                    tmptime = scrollh > 300 ? 500 : 200;
+                //console.log(newPosition);
+                if(offset.top > 150 ){
+                    newPosition += 20;
+                }
+                newPosition += 38;
+                $bar.stop().animate({ marginTop: newPosition + 'px' }, tmptime);
+
+            }else{
+                $bar.stop().animate({ marginTop: 0 }, 300);
+            }
+        }
+        
     });
+
     $('#to-top, #to-top-xs').on( 'click', function( event ){
         $('html,body').animate({scrollTop: '0px'}, 500);
     });
@@ -22,4 +59,5 @@ $(document).ready(function(){
     }, function(){
         $('#to-top-title, #to-top-title-xs').hide();
     });
+
 });
